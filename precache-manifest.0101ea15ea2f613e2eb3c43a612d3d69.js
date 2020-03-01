7b96abfdb0b7b49cc0bcd299f313688c7c921148
Solomon Ayoola <ayoola.solomon89@gmail.com>
2020-03-01 18:42:20 +0100

    Updates

---
+++ precache-manifest.0101ea15ea2f613e2eb3c43a612d3d69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d023a864013f8caba962d1364a165638",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf4d60e8b67f46f02e17",
+    "url": "/static/css/2.8dd801da.chunk.css"
+  },
+  {
+    "revision": "bf4d60e8b67f46f02e17",
+    "url": "/static/js/2.cb188e8f.chunk.js"
+  },
+  {
+    "revision": "00bc537a239fe61cc054",
+    "url": "/static/js/3.8ac7a8ce.chunk.js"
+  },
+  {
+    "revision": "9d1cba0bba61cb9fd95d",
+    "url": "/static/js/4.6fabdcd4.chunk.js"
+  },
+  {
+    "revision": "bfddd0c71fea042da550",
+    "url": "/static/js/main.39b685ad.chunk.js"
+  },
+  {
+    "revision": "f7434cf00d219504860c",
+    "url": "/static/js/runtime~main.31ff046f.js"
+  }
+]);
